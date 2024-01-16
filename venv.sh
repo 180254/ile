4d2519caeea60fea320ab2bd19771f99ae6a7746
Adrian Pedziwiatr <180254@users.noreply.github.com>
2024-01-16 15:45:59 +0100

    weather: add module

--- venv.sh
+++ venv.sh
@@ -4,7 +4,7 @@ venv/bin/pip3 install --upgrade pip wheel setuptools
 
 venv/bin/pip3 install --disable-pip-version-check --upgrade -r requirements-dev.txt
 
-for subproject in ile_shellyscraper ile_tcp_cache ile_openrazer_info qdb_count_rows qdb_wal_switch; do
+for subproject in ile_shellyscraper ile_tcp_cache ile_openrazer_info qdb_count_rows qdb_wal_switch ile_weather_info; do
   if [[ -f "$subproject/requirements.txt" ]]; then
     venv/bin/pip3 install --disable-pip-version-check --upgrade -r $subproject/requirements.txt
   fi
