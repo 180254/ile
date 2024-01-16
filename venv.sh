d09b08056f6e919081c5fdd005f28596d16d311d
Adrian Pedziwiatr <180254@users.noreply.github.com>
2024-01-16 15:40:35 +0100

    venv: cleanup

--- venv.sh
+++ venv.sh
@@ -2,11 +2,14 @@
 python3 -m venv venv
 venv/bin/pip3 install --upgrade pip wheel setuptools
 
+venv/bin/pip3 install --disable-pip-version-check --upgrade -r requirements-dev.txt
+
 for subproject in ile_shellyscraper ile_tcp_cache ile_openrazer_info qdb_count_rows qdb_wal_switch; do
-    if [[ -f "$subproject/requirements.txt" ]]; then
-      venv/bin/pip3 install --disable-pip-version-check --upgrade -r $subproject/requirements.txt
-    fi
-    if [[ -f "$subproject/requirements-dev.txt" ]]; then
-      venv/bin/pip3 install --disable-pip-version-check --upgrade -r $subproject/requirements-dev.txt
-    fi
+  if [[ -f "$subproject/requirements.txt" ]]; then
+    venv/bin/pip3 install --disable-pip-version-check --upgrade -r $subproject/requirements.txt
+  fi
+
+  if [[ -f "$subproject/requirements-dev.txt" ]]; then
+    venv/bin/pip3 install --disable-pip-version-check --upgrade -r $subproject/requirements-dev.txt
+  fi
 done
