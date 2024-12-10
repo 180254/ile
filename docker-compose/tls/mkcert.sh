e0bdfc113f429aca711ed1eb0b0d3745b63c75b1
Adrian Pedziwiatr <180254@users.noreply.github.com>
2024-12-10 11:20:02 +0100

    chore: cleanup

--- docker-compose/tls/mkcert.sh
+++ docker-compose/tls/mkcert.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v cfssl)" ]; then
-  >&2 echo 'err: cfssl is not installed (https://github.com/cloudflare/cfssl)'
+  echo >&2 'err: cfssl is not installed (https://github.com/cloudflare/cfssl)'
   exit 1
 fi
 
+pushd "prod" || (echo 'err: no prod directory' && exit 1)
+
 if [ ! -f .passphrase ]; then
-  LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 32 | head -n 1 > .passphrase
+  LC_ALL=C tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1 >.passphrase
 fi
 
 PASSPHRASE=$(cat .passphrase)
@@ -17,9 +19,11 @@ if [ ! -f ca.pem ]; then
 fi
 
 for i in cloudserver homeserver; do
-  cfssl gencert -profile www -ca ca.pem -ca-key ca-key.pem "$i-csr.json"  | cfssljson -bare "$i"
+  cfssl gencert -profile www -ca ca.pem -ca-key ca-key.pem "$i-csr.json" | cfssljson -bare "$i"
   openssl ec -in "$i-key.pem" -out "$i-key-enc.pem" -aes256 -passout "pass:$PASSPHRASE"
-  cat "$i.pem" "$i-key.pem" > "$i-full.pem"
+  cat "$i.pem" "$i-key.pem" >"$i-full.pem"
 done
 
 echo "passphrase: $PASSPHRASE"
+
+popd || exit 1
